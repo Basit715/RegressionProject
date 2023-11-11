e3863bf19504dd9701dc5d9626e45e2e9877206c
Basit715 <basitabass27411@gmail.com>
2023-11-11 15:38:27 +0530

    change

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+     name="DiamondPricePrediction",
+     version="0.0.1",
+     author="Basit pushoo",
+     author_email="basitabass27411@gmail.com",
+     packages=find_packages()
+)
